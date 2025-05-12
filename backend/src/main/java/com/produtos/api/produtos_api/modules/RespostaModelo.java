5aff2e0cee187b6a7572754576b29decb49512c5
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2025-05-12 11:25:14 -0300

    Criado reposta modelo

---
+++ backend/src/main/java/com/produtos/api/produtos_api/modules/RespostaModelo.java
@@ -0,0 +1,13 @@
+package com.produtos.api.produtos_api.modules;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class RespostaModelo {
+    private String mensagem;
+}
