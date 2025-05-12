52c1bac285889ece9418061e6afc46fdf87a5f4f
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2025-05-12 12:49:11 -0300

    A criação da rota listar no produto controle

--- backend/src/main/java/com/produtos/api/produtos_api/controller/ProdutoControle.java
+++ backend/src/main/java/com/produtos/api/produtos_api/controller/ProdutoControle.java
@@ -1,14 +1,25 @@
 package com.produtos.api.produtos_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.api.produtos_api.modules.ProdutoModelo;
+import com.produtos.api.produtos_api.service.ProdutoServico;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//Conecta o java ao navegador web
 @RestController
 public class ProdutoControle {
+    @Autowired
+    private ProdutoServico ps;
 
     @GetMapping("/")
     public String rota() {
         return "API está funcionando no navegador WEB!";
+    }
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
     }
 }
